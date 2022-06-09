45a689807f1c705f023c83aca5b491e636946675
Bakumenko <tinyy11@gmail.com>
2022-06-09 15:04:16 +0300

    1

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./Movies.css";
 import Header from "../Header/Header";
 import { Link, Route, Routes } from "react-router-dom";
+import SearchForm from './../SearchForm/SearchForm';
 export default function Movies() {
    return (
       <div className="movies">
+         <SearchForm />
       </div>
    );
 }
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SearchForm.css";
+import iconButton from "../../images/button_icon.svg";
+export default function SearchForm() {
+   return (
+      <div className="search-form">
+         <form className="search-form__form">
+            <input
+               type="text"
+               className="search-form__input"
+               placeholder="Фильм"
+            />
+            <button className="search-form__button">
+               <img src={iconButton} alt="Иконка лупы" />
+            </button>
+         </form>
+      </div>
+   );
+}
