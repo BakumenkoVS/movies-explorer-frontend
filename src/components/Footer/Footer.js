8fb1be5e330ee7b4cf642d30887ecda3ca9e5cd1
Bakumenko <tinyy11@gmail.com>
2022-06-09 10:16:03 +0300

    add footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,9 @@ import './Footer.css'
 export default function Footer() {
    return <div className='footer'>
       <h2 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-      <a href="#" className='footer__link'>Яндекс.Практикум</a>
-      <a href="#" className='footer__link'>Github</a>
-      <a href="#" className='footer__link'>Telegram</a>
+      <a href="https://practicum.yandex.ru/" className='footer__link'>Яндекс.Практикум</a>
+      <a href="https://github.com/BakumenkoVS" className='footer__link'>Github</a>
+      <a href="https://t.me/BakumenkoVS" className='footer__link'>Telegram</a>
       <p className='footer__year'>&copy;2020</p>
    </div>;
 }
