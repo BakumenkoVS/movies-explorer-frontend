8fde2ea9716d0451b1fd90b5a0eaa747e183ce66
Bakumenko <tinyy11@gmail.com>
2022-06-09 09:22:21 +0300

    add component footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export default function Footer() {
+   return <div className='footer'>
+      <h2 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <a href="#" className='footer__link'>Яндекс.Практикум</a>
+      <a href="#" className='footer__link'>Github</a>
+      <a href="#" className='footer__link'>Telegram</a>
+      <p className='footer__year'>&copy;2020</p>
+   </div>;
+}
