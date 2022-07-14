3aee6e60ab8e5fc4498b534e7d3308bcb5c99020
vlad bakumenko <tinyyy11@yandex.ru>
2022-07-14 20:55:56 +0300

    add checkbox change

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./FilterCheckbox.css";
-export default function FilterCheckbox() {
+export default function FilterCheckbox({ setShortcut, shortcut }) {
+   function changeCheckbox() {
+      shortcut ? setShortcut(false) : setShortcut(true);
+   }
    return (
       <div className="filter-checkbox">
          <h2 className="filter-checkbox__title">Короткометражки</h2>
@@ -9,6 +12,8 @@ export default function FilterCheckbox() {
                type="checkbox"
                className="filter-checkbox__input"
                id="filter-checkbox__input"
+               onChange={changeCheckbox}
+               checked={shortcut}
             />
             <label
                for="filter-checkbox__input"
