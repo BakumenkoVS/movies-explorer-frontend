4bc319c91caf2e01e58a6691ff0a985fd49c9dd0
vlad bakumenko <tinyyy11@yandex.ru>
2022-07-14 09:27:09 +0300

    Переделал фильтрацию фильмов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,26 +19,45 @@ export default function App() {
    const [loggedIn, setLoggedIn] = useState(false);
    const [currentUser, setCurrentUser] = useState(null);
    const [movies, setMovies] = useState(null);
+   const [moviesSaved, setMoviesSaved] = useState(null);
+   const [shortcut, setShortcut] = useState(false);
+   const [shortcutSave, setShortcutSave] = useState(false);
+   const [searchMovies, setSearchMovies] = useState([]);
+   const [searchSavedMovies, setSearchSavedMovies] = useState([]);
+
    const history = useNavigate();
 
+   //инициализация проверки токена
    useEffect(() => {
       tokenCheck();
    }, []);
 
+   //При изменении state login переадресует на страницу movies
    useEffect(() => {
       if (loggedIn) {
          history("/movies");
       }
    }, [loggedIn]);
 
+   useEffect(() => {
+      setShortcut(JSON.parse(localStorage.getItem("shortcut")));
+      setSearchMovies(JSON.parse(localStorage.getItem("foundFilms")));
+      setMoviesSaved(JSON.parse(localStorage.getItem("moviesSave")));
+   }, []);
+
    //handlers
+   //Открытие меню
    function handleEditMenuOpen() {
       setIsMenuOpen(true);
    }
+
+   //закрытие меню
    function handleEditMenuClose() {
       setIsMenuOpen(false);
    }
 
+   //handler регистрации  получает пароль логин и имя
+   //перенаправляет на страницу логина
    function handleRegister(password, email, name) {
       return api
          .signUp(password, email, name)
@@ -50,6 +69,8 @@ export default function App() {
          });
    }
 
+   //Handler обновления информации о пользователе получает name и email
+   // отдает обновленную информацию о пользователе
    const handleUpdateUser = (name, email) => {
       api.addUserInfo(name, email)
          .then((res) => {
@@ -58,6 +79,7 @@ export default function App() {
          .catch((err) => console.log(err));
    };
 
+   //Handler login получает password и email отлает jwt токен
    function handleLogin(password, email) {
       return api
 
@@ -74,11 +96,14 @@ export default function App() {
          });
    }
 
+   //Функция выхода из профиля
    function signOut() {
       localStorage.removeItem("jwt");
       setLoggedIn(false);
    }
 
+   //функция проверяет наличия токена в локальном хранилище и если он есть
+   //разрешает вход без авторизации
    function tokenCheck() {
       let jwt = localStorage.getItem("jwt");
       if (jwt) {
@@ -91,13 +116,62 @@ export default function App() {
       }
    }
 
+   //Функция фильтрует исходный массив фильмов
+   //по продолжительности и ключевому набору букв
+   function filterMovies(moviesData, inputInfo, duration) {
+      return moviesData.filter((item) => {
+         return duration
+            ? item.nameRU
+                 .toLowerCase()
+                 .includes(`${inputInfo.toLowerCase()}`) && item.duration < 40
+            : item.nameRU.toLowerCase().includes(`${inputInfo.toLowerCase()}`);
+      });
+   }
+
+   //функция получает inputInfo из формы поиска и фильтрует массив всех фильмов
+   //по ключевому слову и продолжительности так же записывает результаты в локальное хранилище
+   const foundFilms = (inputInfo) => {
+      debugger;
+      const newFilterMovies = filterMovies(movies, inputInfo, shortcut);
+      console.log(1);
+      console.log(newFilterMovies);
+      setSearchMovies(newFilterMovies);
+      localStorage.setItem("foundFilms", JSON.stringify(newFilterMovies));
+      localStorage.setItem("inputInfo", JSON.stringify(inputInfo));
+      localStorage.setItem("shortcut", JSON.stringify(shortcut));
+   };
+
+   //Фильтрация массива сохраненных фильмов
+   const foundSavedFilms = (inputInfo) => {
+      debugger;
+      const newFilterMovies = filterMovies(
+         moviesSaved,
+         inputInfo,
+         shortcutSave
+      );
+      console.log(2);
+      console.log(newFilterMovies);
+      setSearchSavedMovies(newFilterMovies);
+   };
+
+   //Обращение к api для получения информации о пользователе  массива фильмов
+   //а так же сохраненных фильмов
    useEffect(() => {
       if (loggedIn) {
-         Promise.all([api.getContent(), moviesApi.getMovies()])
-
-            .then(([user, movies]) => {
+         Promise.all([
+            api.getContent(),
+            moviesApi.getMovies(),
+            api.getSavedMovies(),
+         ])
+            .then(([user, movies, moviesSaved]) => {
                setCurrentUser(user.data);
                setMovies(movies);
+               const userMovies = moviesSaved.filter(
+                  (movie) => movie.owner === currentUser._id
+               );
+               setMoviesSaved(userMovies);
+               localStorage.setItem("moviesSave", JSON.stringify(userMovies));
+               localStorage.setItem("movies", JSON.stringify(movies));
             })
             .catch((err) => console.log(err));
       }
@@ -117,7 +191,10 @@ export default function App() {
                            isOpen={isMenuOpen}
                            onEditMenu={handleEditMenuOpen}
                            loggedIn={loggedIn}
-                           onMovies={movies}
+                           onMovies={searchMovies}
+                           setShortcut={setShortcut}
+                           shortcut={shortcut}
+                           foundFilms={foundFilms}
                         />
                      </ProtectedRoute>
                   }
@@ -131,6 +208,9 @@ export default function App() {
                            isOpen={isMenuOpen}
                            onEditMenu={handleEditMenuOpen}
                            loggedIn={loggedIn}
+                           setShortcut={setShortcutSave}
+                           shortcut={shortcutSave}
+                           foundFilms={foundSavedFilms}
                         />
                      </ProtectedRoute>
                   }
