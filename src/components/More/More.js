e143181c5559d492676507da6f9f78f91c16e2d8
vlad bakumenko <tinyyy11@yandex.ru>
2022-07-15 18:34:08 +0300

    add more hide

--- src/components/More/More.js
+++ src/components/More/More.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./More.css";
-export default function More({onClick}) {
+export default function More({ onClick, onMoviesSlice, onMovies }) {
+   const [full, setFull] = useState(false);
+console.log(onMovies.length)
+console.log(onMoviesSlice.length)
+
+useEffect(()=>{
+   if(onMovies.length == onMoviesSlice.length ) {
+      debugger
+      setFull(true)
+   } else {
+      setFull(false)
+   }
+},[onMoviesSlice])
+   
+
+   const MoreClassName = `${
+      full ? "more__disable" : "more"
+   }`;
+
    return (
-      <div className="more">
-         <button className="more__button" onClick={onClick}>Ещё</button>
+      <div className={MoreClassName}>
+         <button className="more__button" onClick={onClick}>
+            Ещё
+         </button>
       </div>
    );
 }
